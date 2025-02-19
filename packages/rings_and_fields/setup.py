5ca89ceef203b7d539ae6c90c3871b40de5c84b4
Thomas Huettemann <thuette+github@googlemail.com>
2025-02-19 20:22:20 +0000

    drt

--- packages/rings_and_fields/setup.py
+++ packages/rings_and_fields/setup.py
@@ -6,9 +6,9 @@ setup(
     description="Rings and Fields module",
     author="Thomas Huettemann",
     author_email='t.huettemann@qub.ac.uk',
-    url="https://github.com/t-huettemann/MTH4021-repository/packages/rings_and_fields",
+    #url="https://github.com/t-huettemann/MTH4021-repository/packages/rings_and_fields",
     python_requires='>=3.7',
-    modules=["rings_and_fields"]
+    py_modules=["rings_and_fields"],
     #packages=["rings_and_fields", "abelian_groups", "elliptic_curves"],
     #package_dir={"rings_and_fields":"packages/rings_and_fields", "abelian_groups":"packages/abelian_groups", "elliptic_curves":"packages/elliptic_curves"},
     install_requires=['gmpy2', 'primefac']
