6f2805b522cf7c67f18ff1e4212e1354f9557eab
Thomas Huettemann <thuette+github@googlemail.com>
2025-02-19 17:52:26 +0000

    Making rings_and_fields a Python package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='rings_and_fields',
@@ -6,8 +6,9 @@ setup(
     description="Rings and Fields package",
     author="Thomas Huettemann",
     author_email='t.huettemann@qub.ac.uk',
-    url="https://github.com/t-huettemann/MTH4021-repository.git",
+    url="https://github.com/t-huettemann/MTH4021-repository/",
     python_requires='>=3.7',
-    packages=find_packages(),
+    packages=["rings_and_fields"],
+    package_dir={"rings_and_fields":"packages/rings_and_fields"},
     install_requires=['gmpy2', 'primefac']
 )
