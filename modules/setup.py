a91b2e643d68a41e265eb5347d3b4809d19bdc6e
Thomas Huettemann <thuette+github@googlemail.com>
2025-02-20 12:06:50 +0000

    2nd cookbook page added

---
+++ modules/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MTH4021'
+    version='0.1.0',
+    description="Various Python modules for use with MTH4021",
+    author="Thomas Huettemann",
+    author_email='t.huettemann@qub.ac.uk',
+    python_requires='>=3.7',
+    py_modules=["rings_and_fields", "abelian_groups", "elliptic_curves"],
+    install_requires=['gmpy2', 'primefac']
+)
